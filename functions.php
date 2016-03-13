2620f78f04ececfcee39346afaf978f2ca89ca88
Bisma Ayyaz <bismaayyaz_19@live.com>
2016-03-13 15:52:22 -0400

    Linking "Testing Options.php"

--- functions.php
+++ functions.php
@@ -154,3 +154,9 @@ require get_template_directory() . '/inc/customizer.php';
  * Load Jetpack compatibility file.
  */
 require get_template_directory() . '/inc/jetpack.php';
+
+/**
+* Linking options page.
+*/
+require get_stylesheet_directory() .'/inc/options.php';
+
