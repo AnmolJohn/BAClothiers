a5204d74d025947b2bdf41c220538ad624898b6e
Bisma Ayyaz <bismaayyaz_19@live.com>
2016-03-15 16:15:20 -0400

    font-color
    asdasd

--- header.php
+++ header.php
@@ -21,7 +21,7 @@
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="site">
+<div style="font-color:red;" id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'ba-clotheirs' ); ?></a>
 
 	<header id="masthead" class="site-header" role="banner">
