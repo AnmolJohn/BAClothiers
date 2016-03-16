57e445691dfc39ddfbf7013abf1f5a3b31d5032a
AnmolJJohn <johnanm@sheridancollege.ca>
2016-03-16 19:38:26 -0400

    comments

--- header.php
+++ header.php
@@ -47,7 +47,9 @@
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'ba-clotheirs' ); ?></button>
 			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 		</nav><!-- #site-navigation -->
-       
+     
+
+     <!-- this code-->  
        <?php
        $args = array(
             'post_type' => 'seasons',
