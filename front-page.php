84b293e1d8c9872136e77867b6a4992e7c32f109
Anjanie <anjanie.rupnarain@mail.utoronto.ca>
2016-03-17 01:16:19 -0400

    Revert "Changes to the front page and styles"
    This reverts commit 8e0dd22d58a2b8dcfc0b87c0dab9a11efea6a856.

--- front-page.php
+++ front-page.php
@@ -9,3 +9,35 @@
 
 get_header(); ?>
 
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+<!--/-->
+
+			
+	<?php
+      $bacpage = ( get_query_var ( 'paged' ) ) ? get_query_var ( 'paged' ) : 1;
+      $bac = array('showposts'=>2, 'cat'=>'192','paged'=>$bacpage);
+                  $bac_query = new WP_Query($bac);
+              ?>
+
+                  
+            <?php if ($bac_query->have_posts()) : while ($bac_query->have_posts()) : $bac_query->the_post(); ?>
+              <article id="post-<?php the_ID();?>" <?php post_class(); ?>>
+
+                    <?php if ( has_post_thumbnail() ) : ?>
+    <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+        <?php the_post_thumbnail('large'); ?>
+    </a>
+<?php endif; ?>
+
+               </article><!-- #post-## -->
+               
+       <?php endwhile; endif; ?>
+             
+              
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
