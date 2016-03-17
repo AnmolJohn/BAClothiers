a4534edf66447f7244cf7ca3fb59c1d7b8ad02a4
Bisma Ayyaz <bismaayyaz_19@live.com>
2016-03-17 00:17:50 -0400

    Options Page Change

--- inc/options.php
+++ inc/options.php
@@ -6,72 +6,50 @@
 	
 function bac_add_submenu() {
 		add_submenu_page( 'themes.php', 'BA Clotheirs Options Page', 'BAC Theme Options', 'manage_options', 'theme_options', 'bac_theme_options_page');
-	}
-add_action( 'admin_menu', 'bac_add_submenu' );
+	     }
+      add_action( 'admin_menu', 'bac_add_submenu' );
 	
 
 function bac_settings_init() { 
-	register_setting( 'BAC_theme_options', 'bac_options_settings' );
+	     register_setting( 'BAC_theme_options', 'bac_options_settings' );
 	
-	add_settings_section(
-		'bac_options_page_section', 
-		'This page will allow the users to change few things directly from the BA Clotheirs options page', 
-		'bac_options_page_section_callback', 
-		'BAC_theme_options'
-	);
+	add_settings_section('bac_options_page_section','Feel free to customize the theme','bac_options_page_section_callback','BAC_theme_options');
 	
 	function bac_options_page_section_callback() { 
-		echo 'A description and detail about the section.';
-	}
+		echo 'Enjoy trying different colors and change daily greetings.';
+	     }
 	
-/**
- * To enter greeting text.
- */
-	add_settings_field( 
-		'bac_text_field', 
-		'Enter Daily Greetings/Promotions', 
-		'bac_text_field_render', 
-		'BAC_theme_options', 
-		'bac_options_page_section' 
-	);
-
-	add_settings_field( 
-		'cd_font_field', 
-		'Change Font color', 
-		'cd_font_field_render', 
-		'cd_theme_options', 
-		'cd_options_page_section'  
-	);
-
+	
 /**
  * For changing background color.
  */
-	add_settings_field( 
-		'bac_radio_field', 
-		'Change Background Color', 
-		'bac_radio_field_render', 
-		'BAC_theme_options', 
-		'bac_options_page_section'  
-	);
+	add_settings_field('bac_radio_field', 'Change Background Color', 'bac_radio_field_render', 'BAC_theme_options', 'bac_options_page_section' );
 	
-	add_settings_field( 
-		'cd_sitewidth_field', 
-		'Enter content in the textarea', 
-		'cd_sitewidth_field_render', 
-		'theme_options', 
-		'cd_options_page_section'  
-	);
+
+/**
+ * To enter greeting text.
+ */
+	add_settings_field('bac_text_field','Enter Daily Greetings/Promotions', 'bac_text_field_render', 'BAC_theme_options', 'bac_options_page_section');
+
+
 
 /**
  * Adding Font Color Change option
  */	
-	add_settings_field( 
-		'bac_select_field', 
-		'Choose Font Color', 
-		'bac_select_field_render', 
-		'BAC_theme_options', 
-		'bac_options_page_section'  
-	);
+	add_settings_field('bac_select_field', 'Choose Font Color','bac_select_field_render','BAC_theme_options','bac_options_page_section' );
+
+
+/**
+ * For changing background color.
+ */	
+	function bac_radio_field_render() { 
+		$options = get_option( 'bac_options_settings' );
+		?>
+		<input type="radio" name="bac_options_settings[bac_radio_field]" <?php if (isset($options['bac_radio_field'])) checked( $options['bac_radio_field'], 1 ); ?> value="1" /> <label>Color One</label><br />
+		<input type="radio" name="bac_options_settings[bac_radio_field]" <?php if (isset($options['bac_radio_field'])) checked( $options['bac_radio_field'], 2 ); ?> value="2" /> <label>Color Two</label><br />
+		<input type="radio" name="bac_options_settings[bac_radio_field]" <?php if (isset($options['bac_radio_field'])) checked( $options['bac_radio_field'], 3 ); ?> value="3" /> <label>Color Three</label>
+		<?php
+	}
 
 /**
  * To enter greeting text.
@@ -96,45 +74,16 @@ function bac_settings_init() {
 		</select>
 	<?php
 	}
-
-/**
- * For changing background color.
- */	
-	function bac_radio_field_render() { 
-		$options = get_option( 'bac_options_settings' );
-		?>
-		<input type="radio" name="bac_options_settings[bac_radio_field]" <?php if (isset($options['bac_radio_field'])) checked( $options['bac_radio_field'], 1 ); ?> value="1" /> <label>Color One</label><br />
-		<input type="radio" name="bac_options_settings[bac_radio_field]" <?php if (isset($options['bac_radio_field'])) checked( $options['bac_radio_field'], 2 ); ?> value="2" /> <label>Color Two</label><br />
-		<input type="radio" name="bac_options_settings[bac_radio_field]" <?php if (isset($options['bac_radio_field'])) checked( $options['bac_radio_field'], 3 ); ?> value="3" /> <label>Color Three</label>
-		<?php
-	}
-	
-	function bac_sitewidth_field_render() { 
-		$options = get_option( 'bac_options_settings' );
-		?>
-		<textarea cols="40" rows="5" name="bac_options_settings[bac_sitewidth_field]"><?php if (isset($options['bac_sitewidth_field'])) echo $options['bac_sitewidth_field']; ?></textarea>
-		<?php
-	}
-
-	function cd_select_field_render() { 
-		$options = get_option( 'cd_options_settings' );
-		?>
-		<select name="cd_options_settings[cd_select_field]">
-			<option value="1" <?php if (isset($options['cd_select_field'])) selected( $options['cd_select_field'], 1 ); ?>>Option 1</option>
-			<option value="2" <?php if (isset($options['cd_select_field'])) selected( $options['cd_select_field'], 2 ); ?>>Option 2</option>
-		</select>
-	<?php
-	}
 	
 	function bac_theme_options_page(){ 
 		?>
 		
 		<?php
-		   settings_errors ();
+		      settings_errors ();
 		?>
 		
 		<form action="options.php" method="post">
-			<h2>BA Clotheirs Options Page</h2>
+			<h2>BAC Options Page</h2>
 			<?php
 			settings_fields( 'BAC_theme_options' );
 			do_settings_sections( 'BAC_theme_options' );
