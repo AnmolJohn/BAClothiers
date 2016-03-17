b40223e8c9655204ad3f58e77eb437624c66b0b1
Bisma Ayyaz <bismaayyaz_19@live.com>
2016-03-17 00:41:25 -0400

    hamburger Menu

--- header.php
+++ header.php
@@ -48,7 +48,7 @@
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'ba-clotheirs' ); ?></button>
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( '≡', 'ba-clotheirs' ); ?></button>
 			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 		</nav><!-- #site-navigation -->
      
