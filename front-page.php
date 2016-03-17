e78a38be9bc84f9b0ef01bd5fd7b9c114462ce5c
Anjanie <anjanie.rupnarain@mail.utoronto.ca>
2016-03-17 01:14:56 -0400

    change

--- front-page.php
+++ front-page.php
@@ -9,3 +9,24 @@
 
 get_header(); ?>
 
+<?php
+       $args = array(
+            'post_type' => 'seasons',
+            'post_per_page' => -1
+       	);
+       
+       $seasons = new WP_Query( $args );
+
+       if ( $seasons->have_posts() ): ?>
+       
+	       <div class="flexslider">
+	 			 <ul class="slides">
+	           <?php while( $seasons->have_posts() ): $seasons->the_post(); ?>
+	     		<li>
+	     		<?php the_post_thumbnail( 'seasons' ); ?>
+	   			</li>
+	   			<?php endwhile; ?>
+	 			 </ul>
+			</div>
+
+      <?php endif; ?>
